fd79e66d5ba100b2240b32edeae0103b492db267
newsoft111 <ydj0617@gmail.com>
2022-11-03 17:42:22 +0900

    d

--- order/views.py
+++ order/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from .models import *
 from django.core.paginator import Paginator
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+from django.views.generic import View
+import pandas as pd
 # Create your views here.
 
 @login_required(login_url="account:login")
@@ -27,25 +29,55 @@ def order_list(request):
 	})
 
 
-@login_required(login_url="account:login")
-def order_preview(request):
-	order_preview_json = {"order_preview":''}
-	order_preview_list = []
-	today = datetime.now()
+class OrderPreview(View):
+	def get(self, request):
+		self.end_date = datetime.now() + timedelta(1)
 
-	q = Q()
-	q &= Q(product__user=request.user)
-	if request.POST.get('start_date'):
-		start_date = datetime.strptime(request.GET.get("start_date"), "%Y-%m-%d")
-	else:
-		start_date = today - timedelta(days=7)
-	q &= Q(order__paid_at__range = [start_date, today])
+		q = Q()
+		q &= Q(product__user=request.user)
+		if request.POST.get('start_date'):
+			self.start_date = datetime.strptime(request.GET.get("start_date"), "%Y-%m-%d")
+		else:
+			self.start_date = self.end_date - timedelta(days=7)
+		q &= Q(order__paid_at__range = [self.start_date, self.end_date])
 
-	order_items = OrderItem.objects.filter(q)
+		order_items = OrderItem.objects.filter(q)
 
-	total_price = 0
-	for items in order_items:
-		total_price += items.sub_total_price()
+		if request.GET.get('standard') == 'Monthly':
+			self.date_format = "%b"
+			chart_data = self.monthly()
+		else:
+			self.date_format = "%a"
+			chart_data = self.weekly()
+			
+		total_price = 0
+		for items in order_items:
+			chart_data[items.order.paid_at.strftime(self.date_format)] += 1
+			total_price += items.sub_total_price()
+			
+			
+		return JsonResponse({
+			"chart_data": [{"x": key, "y": value} for key, value in chart_data.items()],
+			'count': order_items.count()
+		})
 
-	order_preview_json['total_price'] = format(total_price, ',')
-	return JsonResponse(order_preview_json)
+	def weekly(self):
+		preview_data = {}
+		def daterange(start_date, end_date):
+			for n in range(int((end_date - start_date).days)):
+				yield self.start_date + timedelta(n)
+
+		for single_date in daterange(self.start_date, self.end_date):
+			preview_data[single_date.strftime(self.date_format)] = 0
+			
+		return preview_data
+
+	def monthly(self):
+		preview_data = {}
+		month_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+		for month in month_list:
+			preview_data[month] = 0
+
+		return preview_data
+
