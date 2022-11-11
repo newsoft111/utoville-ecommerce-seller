fbf019a9e326567d2789c9ba13ad88202632d32c
newsoft111 <ydj0617@gmail.com>
2022-11-11 16:23:49 +0900

    ㄴㅇㄹ

--- profit/views.py
+++ profit/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from .models import *
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+import json, re
 
-# Create your views here.
+def profit_list(request):
+	profit_objs = Profit.objects.filter(seller=request.user)
+	return render(request, 'profit/profit_list.html', {
+		"profit_objs": profit_objs
+	})
