3b0a12210f43a70c45c04d11a49f928ce12951c8
newsoft111 <ydj0617@gmail.com>
2022-11-14 13:33:02 +0900

    p

--- profit/views.py
+++ profit/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+from django.db.models import Q, Sum
 from .models import *
 from django.core.paginator import Paginator
 from django.http import JsonResponse
@@ -8,6 +8,13 @@ import json, re
 
 def profit_list(request):
 	profit_objs = Profit.objects.filter(seller=request.user)
+	profit_amount = profit_objs.aggregate(Sum('profit_amount'))['profit_amount__sum']
+	if profit_amount is None:
+		profit_amount = 0
+	profit_done_objs = ProfitDone.objects.filter(seller=request.user)
+
 	return render(request, 'profit/profit_list.html', {
-		"profit_objs": profit_objs
+		"profit_objs": profit_objs,
+		'profit_amount': profit_amount,
+		'profit_done_objs': profit_done_objs
 	})
