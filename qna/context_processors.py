f99fda872fa057e1cb9dc4dc95b476dd14e3f2c2
newsoft111 <ydj0617@gmail.com>
2022-11-24 15:56:09 +0900

    pp

--- qna/context_processors.py
+++ qna/context_processors.py
@@ -3,9 +3,11 @@ from django.db.models import Q
 
 
 def counter_new_qna(request):
-	q = Q()
-	q &= Q(product__user = request.user)
-	q &= Q(answered_at = None)
-	result = ProductQnA.objects.filter(q).count()
-	print(result)
+	if request.user.is_authenticated: #로그인 상태면
+		q = Q()
+		q &= Q(product__user = request.user)
+		q &= Q(answered_at = None)
+		result = ProductQnA.objects.filter(q).count()
+	else:
+		result = None
 	return {'counter_new_qna':  result}
