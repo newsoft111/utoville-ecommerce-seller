ce907bb9417981267a41c2fa09c7f281e10db1d8
newsoft111 <87762263+newsoft111@users.noreply.github.com>
2022-12-07 13:12:33 +0900

    주문수 문의수 로그인 체크

--- order/context_processors.py
+++ order/context_processors.py
@@ -3,9 +3,12 @@ from django.db.models import Q
 
 
 def counter_new_order(request):
-	q = Q()
-	q &= Q(is_responded=False)
-	q &= Q(product__user = request.user)
-	q &= Q(order__payment__is_paid = True)
-	result = OrderItem.objects.filter(q).count()
+	if request.user.is_authenticated:
+		q = Q()
+		q &= Q(is_responded=False)
+		q &= Q(product__user = request.user)
+		q &= Q(order__payment__is_paid = True)
+		result = OrderItem.objects.filter(q).count()
+	else:
+		result = 0
 	return {'counter_new_order':  result}
--- qna/context_processors.py
+++ qna/context_processors.py
@@ -3,8 +3,11 @@ from django.db.models import Q
 
 
 def counter_new_qna(request):
-	q = Q()
-	q &= Q(product__user = request.user)
-	q &= Q(answered_at = None)
-	result = ProductQnA.objects.filter(q).count()
+	if request.user.is_authenticated:
+		q = Q()
+		q &= Q(product__user = request.user)
+		q &= Q(answered_at = None)
+		result = ProductQnA.objects.filter(q).count()
+	else:
+		result = 0
 	return {'counter_new_qna':  result}
